0a2be5c59e211ab95e8d10b0b5b68fd0d9709ea1
harika12choco <harika.m2005@gmail.com>
2025-05-07 11:57:02 +0530

    Updated code with latest changes to dataframe columns

--- Data-collection/main.py
+++ Data-collection/main.py
@@ -8,14 +8,9 @@ df = pd.read_csv('C:/Users/DELL/Downloads/modified csv.csv')
 print(df.head())
 df.replace(0, np.nan, inplace=True)
 print(df.dtypes)
-print(df[['forgery', 'negligent driving', 'criminal trespass', 'cruelty']].head(10))
-# Convert timestamp column (replace 'timestamp' with your actual column name)
-df['timestamp'] = pd.to_datetime(df['timestamp'])
+print(df[['forgery', 'negligent driving', 'criminal trespass', 'cruelty']])
 
-# Extract hour, weekday, weekend
-df['hour'] = df['timestamp'].dt.hour
-df['weekday'] = df['timestamp'].dt.weekday
-df['weekend'] = df['weekday'].apply(lambda x: 1 if x >= 5 else 0)
+
 
 
 
